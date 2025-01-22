3580440b8209adf2e5a4ec9b8a6f0ac7ad447b4e
Suleman-shah5829 <sulemanshah5829@gmail.com>
2025-01-23 00:30:19 +0500

    remove database connection from server to test vercel working

--- backend/index.js
+++ backend/index.js
@@ -24,18 +24,18 @@ app.use((req, res, next) => {
 });
 
 // Create database connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
 //connect database
-db.connect((err) => {
-  if (err) throw err;
-  else console.log("MYSQL connected Successfully wow:");
-});
+// db.connect((err) => {
+//   if (err) throw err;
+//   else console.log("MYSQL connected Successfully wow:");
+// });
 // console.log("DB_HOST:", process.env.DB_HOST);
 // console.log("DB_USER:", process.env.DB_USER);
 // console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
